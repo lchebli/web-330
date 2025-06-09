11a98e95620a048f6ca0606c92e0cd504dde905e
Leslie Khattarchebli <lkhattarchebli@my365.bellevue.edu>
2025-06-08 21:53:15 -0700

    assignment-done

--- web-330/week-2/character-generator/script.js
+++ web-330/week-2/character-generator/script.js
@@ -3,23 +3,58 @@
   Chapter 1
   Programming Assignment
 
-  Author:
-  Date:
-  Filename:
+  Author: Leslie Khattarchebli
+  Date: 6/8/2025
+  Filename: Closures
 */
 
 "use strict";
 
 function createCharacter(name, gender, characterClass) {
-  // TODO: Implement this function
+  // create character function
+    return {
+        getName: function() {
+            return name;
+        },
+        getGender: function() {
+            return gender;
+        },
+        getClass: function() {
+            return charClass;
+        }
+    };
 }
 
-document.getElementById("generateHero").addEventListener("click", function(e) {
+document.getElementById("generateHero").addEventListener,"click", function(e) {
   e.preventDefault();
 
-  // TODO: Get form values
+function createCharacter(name, gender, charClass) {
+    return {
+        getName: () => name,
+        getGender: () => gender,
+        getClass: () => characterClass
+    };
+}
+}
 
-  // TODO: Create character
 
-  // TODO: Display character information
-});
+// Handle form submission
+document.getElementById("characterForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent page reload
+
+    // Form values
+    const name = document.getElementById("name").value;
+    const gender = document.querySelector('input[name="gender"]:checked').value;
+    const charClass = document.getElementById("class").value;
+
+    // Create character
+    const newCharacter = createCharacter(name, gender, characterClass);
+
+    // Display Character Information
+    document.getElementById("characterDisplay").innerHTML = `
+        <p><strong>Name:</strong> ${newCharacter.getName()}</p>
+        <p><strong>Gender:</strong> ${newCharacter.getGender()}</p>
+        <p><strong>Class:</strong> ${newCharacter.getClass()}</p>
+    `;
+})
+;
